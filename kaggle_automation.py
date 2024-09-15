c8379bb34d660ec7ddd903142691c42b8e9534cd
Vikas Sanwal <vikassnwl@gmail.com>
2024-09-15 20:56:17 +0530

    Add kaggle_automation.py

---
+++ kaggle_automation.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+from tqdm import tqdm
+
+
+class KaggleAutomation:
+    def __init__(self):
+        ...
+
+    def get_urls(self, html_file_path):
+        html_content = open(html_file_path).read()
+        soup = BeautifulSoup(html_content, 'lxml')
+        links = soup.find_all('a')
+        hrefs = []
+        for link in links:
+            href = link.get('href')
+            if href and "scriptVersionId" in href:
+                hrefs.append(href)
+        return hrefs
+
+    def fetch_scores(self, html_file_path, sleep=2):
+        service = Service(executable_path="./chromedriver")
+        driver = webdriver.Chrome(service=service)
+        urls = self.get_urls(html_file_path)
+        self.versions = []
+        self.pub_scores = []
+        for url in tqdm(urls):
+            driver.get(url)
+            time.sleep(sleep)
+            html_content = driver.page_source
+            soup = BeautifulSoup(html_content, 'lxml')
+            try:
+                p_content = soup.findAll('p', class_='sc-dENhDJ sc-lopgLL fTZFV hqBFNM')[1].text
+                span_content = soup.findAll('span', class_='sc-gLXSEc kLwwHW')[-1].text
+            except Exception as e:
+                continue
+            self.versions.append(span_content)
+            self.pub_scores.append(p_content)
+
+        driver.quit()
+
+    def print_scores(self):
+        print(f"{'Notebook Version':20}{'Public Score'}")
+        print(f"{len('Notebook Version')*'-':20}{len('Public Score')*'-'}")
+        for version, pub_score in zip(self.versions, self.pub_scores):
+            print(f"{version:20}{pub_score}")
